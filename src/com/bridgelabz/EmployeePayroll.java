ebe7e3ec26c73705e00d5d0f56fcc542a22045e3
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-10 22:41:37 +0530

    Update EmployeePayroll.java

--- src/com/bridgelabz/EmployeePayroll.java
+++ src/com/bridgelabz/EmployeePayroll.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class EmployeePayroll {
 
     public static void main(String[] args) {
-        EmployeeServices employee = new EmployeeServices();
+        EmployeePayroll employee = new EmployeePayroll();
         Scanner scan = new Scanner(System.in);
 
         System.out.print(" Please enter the name of the employee: ");
@@ -24,7 +24,38 @@ public class EmployeePayroll {
 
         scan.close();
     }
+int id;
+    String name;
+    double salary;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "[id=" + id + ", name=" + name + ", salary=" + salary + "]";
+    }
 }
 
 
