7dfdf81ca34b8fae02793547c315ab94d024f43e
Kiran <kiranhulawale123@gmail.com>
2022-04-10 22:37:24 +0530

    Added UC-1 to read employee payroll details from console

--- src/com/bridgelabz/EmployeePayroll.java
+++ src/com/bridgelabz/EmployeePayroll.java
@@ -1,4 +1,29 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class EmployeePayroll {
+
+    public static void main(String[] args) {
+        EmployeeServices employee = new EmployeeServices();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print(" Please enter the name of the employee: ");
+        String name = scan.next();
+
+        System.out.print(" Please enter the id of the employee: ");
+        int id = scan.nextInt();
+
+        System.out.print(" Please enter the salary of the employee: ");
+        double salary = scan.nextDouble();
+
+        employee.setName(name);
+        employee.setId(id);
+        employee.setSalary(salary);
+
+        System.out.println(employee);
+
+        scan.close();
+    }
+
 }
+
