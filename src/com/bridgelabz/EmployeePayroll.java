f8a5544728b682fc689d64f5a80a20b8eec19902
Kiran <kiranhulawale123@gmail.com>
2022-04-19 11:41:38 +0530

    Added UC-1 to read and write the file

--- src/com/bridgelabz/EmployeePayroll.java
+++ src/com/bridgelabz/EmployeePayroll.java
@@ -1,29 +1,67 @@
 package com.bridgelabz;
+
+import java.util.*;
 import java.util.Scanner;
+
 
 public class EmployeePayroll {
 
-    public static void main(String[] args) {
-        EmployeeServices employee = new EmployeeServices();
-        Scanner scan = new Scanner(System.in);
+    private static List<EmployeeServices> empPayRollList;
+    private static Scanner sc = new Scanner(System.in);
 
-        System.out.print(" Please enter the name of the employee: ");
-        String name = scan.next();
-
-        System.out.print(" Please enter the id of the employee: ");
-        int id = scan.nextInt();
-
-        System.out.print(" Please enter the salary of the employee: ");
-        double salary = scan.nextDouble();
-
-        employee.setName(name);
-        employee.setId(id);
-        employee.setSalary(salary);
-
-        System.out.println(employee);
-
-        scan.close();
+    public EmployeePayroll(List<EmployeeServices> empPayRollList) {
+        this.empPayRollList = empPayRollList;
     }
 
+    public static void main(String[] args) {
+        ArrayList<EmployeeServices> empPayRollList = new ArrayList<EmployeeServices>();
+        EmployeePayroll empPayRollService = new EmployeePayroll(empPayRollList);
+
+        empPayRollService.readData();
+
+        empPayRollService.writeData("Console");
+
+    }
+
+    public static void readData() {
+        System.out.println("Please Enter the following details :");
+        System.out.println("Enter ID");
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter Name :");
+        String name = sc.nextLine();
+        System.out.println("Enter Salary :");
+        double salary = sc.nextDouble();
+
+        EmployeeServices empPayRollObject = new EmployeeServices(id, name, salary);
+        empPayRollList.add(empPayRollObject);
+    }
+
+    public void writeData(String destination) {
+        if (destination.equals("Console"))
+            System.out.println("Employee Pay Roll Data : \n" + empPayRollList.get(0).toString());
+        else if (destination.equals("File"))
+            new EmployeePayRollFileService().writeData(empPayRollList);
+    }
+
+    public int noOfEntries(String destination) {
+        if (destination.equals("Console"))
+            return empPayRollList.size();
+        else if (destination.equals("File"))
+            return new EmployeePayRollFileService().noOfEntries();
+        return 0;
+    }
+
+    public void printData(String destination) {
+        if (destination.equals("Console"))
+            for(EmployeeServices e : empPayRollList)
+            {
+                System.out.println(e.toString()+"\n");
+            }
+        else if (destination.equals("File"))
+            new EmployeePayRollFileService().printData();
+
+
+    }
 }
 
--- src/com/bridgelabz/EmployeeServices.java
+++ src/com/bridgelabz/EmployeeServices.java
@@ -1,37 +1,37 @@
 package com.bridgelabz;
 
 public class EmployeeServices {
+        private int id;
+        private String name;
+        private double salary;
 
-    int id;
-    String name;
-    double salary;
+        public EmployeeServices(int id, String name, double salary) {
+            this.id = this.id;
+            this.name = this.name;
+            this.salary = this.salary;
+        }
 
-    public int getId() {
-        return id;
-    }
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public double getSalary() {
+            return salary;
+        }
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "[id=" + id + ", name=" + name + ", salary=" + salary + "]";
-    }
+        public String toString()
+        {
+            return "id="+id+", name="+name+", salary="+salary;
+        }
 }
